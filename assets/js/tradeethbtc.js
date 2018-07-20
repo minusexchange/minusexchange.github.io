2e0e6d02fcd990bfdaf1a5aee77a1492bc4e97ff
VincentWong <vinymwong@gmail.com>
2018-07-21 03:04:06 +0800

    Homepage modification

--- assets/js/tradeethbtc.js
+++ assets/js/tradeethbtc.js
@@ -1,67 +1,65 @@
- var email=Cookies.get("email");
-    var pass=Cookies.get("password");
-var market=$("#market").val();
-var pair=market.substring(0,3)+'/'+market.substring(3,6);
+var email = Cookies.get("email");
+var pass = Cookies.get("password");
+var market = $("#market").val();
+var pair = market.substring(0, 3) + '/' + market.substring(3, 6);
 
 
 $("#submit-buy").click(function doLogin() {
-    
-        
-        var price=$("#price").val();
-        var amount=$("#amount").val()*price;
-        
-        if(email!="" && pass!="" && price!='' && amount!='')
-        {
-            $.ajax
-            ({
-                type:'post',
-                url:orderbook_servers[0]+'/api/public/trading/shootLimitOrder.php',
-                data:{
-                    email:email,
-                    password:pass,
-                    pair:pair,
-                    price:price,
-                    size:amount
-                },
-                success:function(response) {
-                    $('#alert-trading').html('<div role="alert" class="alert alert-secondary"><span>'+response+'</span></div>');
-                    doMyOrder();
-                },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-    
-            });
-        }
+
+
+    var price = $("#price").val();
+    var amount = $("#amount").val() * price;
+
+    if (email != "" && pass != "" && price != '' && amount != '') {
+        $.ajax
+        ({
+            type: 'post',
+            url: orderbook_servers[0] + '/api/public/trading/shootLimitOrder.php',
+            data: {
+                email: email,
+                password: pass,
+                pair: pair,
+                price: price,
+                size: amount
+            },
+            success: function (response) {
+                $('#alert-trading').html('<div role="alert" class="alert alert-secondary"><span>' + response + '</span></div>');
+                doMyOrder();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+
+        });
+    }
 });
 $("#submit-sell").click(function doBuy() {
-    
-        var market=$("#market").val();
-        var price=$("#price2").val();
-        var amount=$("#amount2").val()*-1;
-        
-        if(email!="" && pass!="" && price!='' && amount!='')
-        {
-            $.ajax
-            ({
-                type:'post',
-                url:orderbook_servers[0]+'/api/public/trading/shootLimitOrder.php',
-                data:{
-                    email:email,
-                    password:pass,
-                    pair:pair,
-                    price:price,
-                    size:amount
-                },
-                success:function(response) {
-                    $('#alert-trading').html('<div role="alert" class="alert alert-secondary"><span>'+response+'</span></div>');
-                    doMyOrder();
-                },
-      error: function (xhr, ajaxOptions, thrownError) {
-        alert(xhr.status);
-        alert(thrownError);
-      }
-            });
-        }
+
+    var market = $("#market").val();
+    var price = $("#price2").val();
+    var amount = $("#amount2").val() * -1;
+
+    if (email != "" && pass != "" && price != '' && amount != '') {
+        $.ajax
+        ({
+            type: 'post',
+            url: orderbook_servers[0] + '/api/public/trading/shootLimitOrder.php',
+            data: {
+                email: email,
+                password: pass,
+                pair: pair,
+                price: price,
+                size: amount
+            },
+            success: function (response) {
+                $('#alert-trading').html('<div role="alert" class="alert alert-secondary"><span>' + response + '</span></div>');
+                doMyOrder();
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert(xhr.status);
+                alert(thrownError);
+            }
+        });
+    }
 });
